62e034d2fe964b9b87a212589c2d6f483c0369a1
Son Tran <son.tran@team.enouvo.com>
2022-01-18 09:05:16 +0700

    Init project

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte';
+</script>
+
+<main>
+  <Counter />
+  <button class="btn btn-primary"> Hello</button>
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button on:click={increment} class="btn btn-success">
+  Clicks: {count}
+</button>
